0170d070b154de32c0135862f25cd7c55ba71770
chasehammock <chasehammock@gmail.com>
2017-11-02 10:23:38 -0500

    Initial commit

---
+++ src/main/java/com/codeup/SpringDemoBlogApplication.java
@@ -0,0 +1,12 @@
+package com.codeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoBlogApplication.class, args);
+	}
+}
